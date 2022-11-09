b713d5ab6a9e98b01de6d697fb4f3a212c963c40
emilytwines <emdelbridge@gmail.com>
2022-11-09 16:30:30 -0500

    added CarSerializer + set cars_list() endpoint

--- cars/models.py
+++ cars/models.py
@@ -8,4 +8,3 @@ class Car(models.Model):
     model = models.CharField (max_length = 255)
     year = models.IntegerField()
     price = models.DecimalField(max_digits = 8, decimal_places = 2)
-    
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def cars_list(request):
+
+    return Response("ok")
