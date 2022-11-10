329fc6a263a3f19207912e36597610bf1bc5def4
emilytwines <emdelbridge@gmail.com>
2022-11-10 11:39:15 -0500

    added new view function for getting by id + added urls path for car_detail() in view

--- cars/views.py
+++ cars/views.py
@@ -13,11 +13,18 @@ def cars_list(request):
         serializer = CarSerializer(cars, many = True)    
         return Response(serializer.data)
 
-    elif request.methd == "POST":
+    elif request.method == "POST":
         serializer = CarSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status.HTTP_201_CREATED)
         
         
-
+@api_view(['GET'])
+def car_detail(request, pk):
+    try:
+        car = Car.objects.get(pk=pk)
+        serializer = CarSerializer(car)
+        return Response(serializer.data)
+    except Car.DoesNotExist:
+        return Response (status=status.HTTP_404_NOT_FOUND)
--- cars_project/urls.py
+++ cars_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/cars", include('cars.urls')),
+    path("api/cars/", include('cars.urls')),
 ]
